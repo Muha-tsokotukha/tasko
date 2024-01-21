0fdb8f5a72a432612fa1134048d9cdb0615f548a
Mukhit Home <vinniedrew7@gmail.com>
2024-01-21 14:08:29 +0600

    feat(store): delete

--- src/feature/index.ts
+++ src/feature/index.ts
@@ -2,3 +2,5 @@ export * from "./Column/Add";
 export * from "./Task/Add";
 export * from "./Column/Edit";
 export * from "./Task/Edit";
+export * from "./Column/Delete";
+export * from "./Task/Delete";
